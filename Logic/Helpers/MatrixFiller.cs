fa15740a6ed5c6df125e10ef24e87f33a90d5874
Leonid Sulin <sulin.leonid@gmail.com>
2023-11-10 02:39:37 +0300

    Add helpers

---
+++ Logic/Helpers/MatrixFiller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Logic
+{
+	public static class MatrixFiller
+	{
+		public static void Fill(IMatrix matrix, int nonzeroItems, double max, double min = 0)
+		{
+			int totalItems = matrix.Rows * matrix.Columns;
+			if (nonzeroItems > totalItems) throw new IndexOutOfRangeException();
+			var randomValues = new double[totalItems];
+
+			var random = new Random();
+			for(int i = 0; i < nonzeroItems; i++)
+			{
+				randomValues[i] = min + random.NextDouble() * (max - min);
+			}
+			for(int i = nonzeroItems; i < totalItems; i++)
+			{
+				randomValues[i] = 0;
+			}
+
+			randomValues = randomValues.OrderBy(_ => random.Next()).ToArray();
+			for(int row = 0; row < matrix.Rows; row++)
+			{
+				for(int col = 0; col < matrix.Columns; col++)
+				{
+					matrix.SetItem(row, col, randomValues[row * matrix.Rows + col]);
+				}
+			}
+		}
+
+		public static void Print(IMatrix matrix)
+		{
+			for(int row = 0; row < matrix.Rows; row++)
+			{
+				for(int col = 0; col < matrix.Columns; col++)
+				{
+					Console.Write($"{matrix.GetItem(row, col)} ");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+		}
+	}
+}
+
---
+++ Logic/Helpers/MatrixStats.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Logic
+{
+	public class MatrixStats
+	{
+		IMatrix matrix;
+
+		public MatrixStats(IMatrix matrix)
+		{
+			this.matrix = matrix;
+			Sum = 0;
+			Max = double.MinValue;
+			NonzeroItems = 0;
+			for(int row = 0; row < matrix.Rows; row++)
+			{
+				for(int col = 0; col < matrix.Columns; col++)
+				{
+					var item = matrix.GetItem(row, col);
+					Sum += item;
+					if (Max < item) Max = item;
+					if (item != 0) NonzeroItems++;
+				}
+			}
+		}
+
+		public double Sum { get; private set; }
+
+		public double Average => Sum / (matrix.Rows * matrix.Columns);
+
+		public double Max { get; private set; }
+
+		public double NonzeroItems { get; private set; }
+
+		public void Print()
+		{
+			Console.WriteLine($"Sum: {Sum}");
+			Console.WriteLine($"Avg: {Average}");
+			Console.WriteLine($"Max: {Max}");
+			Console.WriteLine($"Nonzero: {NonzeroItems}");
+		}
+	}
+}
+

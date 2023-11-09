7479e214ff7586ec8b6ef64f693abcac35e05e22
Leonid Sulin <sulin.leonid@gmail.com>
2023-11-10 02:40:14 +0300

    Complete part 1

--- Logic/Program.cs
+++ Logic/Program.cs
@@ -3,6 +3,18 @@ internal class Program
 {
 	private static void Main(string[] args)
 	{
-		
+		var simpleMatrix = new SimpleMatrix(5, 5);
+		MatrixFiller.Fill(simpleMatrix, 20, 10);
+		MatrixFiller.Print(simpleMatrix);
+		var simpleStats = new MatrixStats(simpleMatrix);
+		simpleStats.Print();
+		Console.WriteLine();
+
+		var scatterMatrix = new ScatterMatrix(5, 5);
+		MatrixFiller.Fill(scatterMatrix, 20, 10);
+		MatrixFiller.Print(scatterMatrix);
+		var scatterStats = new MatrixStats(scatterMatrix);
+		scatterStats.Print();
+		Console.WriteLine();
 	}
 }
